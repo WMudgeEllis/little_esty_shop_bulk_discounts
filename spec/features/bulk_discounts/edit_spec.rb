603270544d9e82a9259fca2e3c0d0725284f3bcb
Weston Ellis <ellisweston112@gmail.com>
2021-09-24 17:47:04 -0600

    Rearrange files

--- spec/features/bulk_discounts/edit_spec.rb
+++ spec/features/bulk_discounts/edit_spec.rb
@@ -1,26 +1,15 @@
 require "rails_helper"
 
-RSpec.describe 'bulk discount show page' do
+RSpec.describe 'bulk discount edit page' do
 
   before :each do
     @merchant1 = Merchant.create!(name: 'Hair Care')
 
     @bulk_discount_1 = BulkDiscount.create!(merchant_id: @merchant1.id, discount: 10, threshold: 30)
 
-    visit merchant_bulk_discount_path(@merchant1, @bulk_discount_1)
+    visit edit_merchant_bulk_discount_path(@merchant1, @bulk_discount_1)
   end
 
-  it 'shows info' do
-    expect(page).to have_content("Percent off: #{@bulk_discount_1.discount}")
-    expect(page).to have_content("Quantity threshold: #{@bulk_discount_1.threshold}")
-  end
-
-
-  it 'links to edit page' do
-    click_link 'edit'
-
-    expect(current_path).to eq(edit_merchant_bulk_discount_path(@merchant1, @bulk_discount_1))
-  end
 end
 # As a merchant
 # When I visit my bulk discount show page
