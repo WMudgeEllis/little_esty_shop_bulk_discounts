9585bc0a24cbff3862a5c40a4d2fcee52fddad12
Weston Ellis <ellisweston112@gmail.com>
2021-09-27 15:29:12 -0600

    Create HolidayService

---
+++ app/poro/holiday_service.rb
@@ -0,0 +1,17 @@
+require "json"
+require "faraday"
+
+class HolidayService
+
+  def parse
+    response = Faraday.get("https://date.nager.at/api/v3/NextPublicHolidays/US")
+    JSON.parse(response.body, symbolize_names: true)
+  end
+
+  def next_three_holidays
+    parse[0..2].map do |holiday|
+      holiday[:name]
+    end
+  end
+end
+require "pry"; binding.pry
