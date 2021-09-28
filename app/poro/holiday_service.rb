c63227ca1fb0dbd27f71af96e20219788a7e704d
Weston Ellis <ellisweston112@gmail.com>
2021-09-28 15:13:52 -0600

    Add holiday date to output

--- app/poro/holiday_service.rb
+++ app/poro/holiday_service.rb
@@ -1,8 +1,4 @@
-require "json"
-require "faraday"
-
 class HolidayService
-
   def parse
     response = Faraday.get("https://date.nager.at/api/v3/NextPublicHolidays/US")
     JSON.parse(response.body, symbolize_names: true)
@@ -10,7 +6,7 @@ class HolidayService
 
   def next_three_holidays
     parse[0..2].map do |holiday|
-      holiday[:name]
+      "Name:#{holiday[:name]} Date: #{holiday[:date]}"
     end
   end
 end
